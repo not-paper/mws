2040d521c28b2e3bfc3b9b98d3a49d8e89fa4019
Brian Van Stone <brianvanstone@gmail.com>
2016-06-07 16:57:53 -0400

    Initial commit of project

---
+++ src/main/java/tech/notpaper/mws/IdentifyCard.java
@@ -0,0 +1,46 @@
+package tech.notpaper.mws;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+
+@Path("identifycard")
+public class IdentifyCard {
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     * @throws IOException 
+     */
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes("*/*")
+    public String identifyCard(InputStream stream) throws IOException {
+    	byte[] image = IOUtils.toByteArray(stream);
+    	
+    	FileUtils.writeByteArrayToFile(new File("uploadedimage.jpg"), image);
+    	
+        return "{\nimage: " + new File(".").getAbsolutePath() + "/uploadimage.jpg\n}";
+    }
+    
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("isalive")
+    public String isAlive() {
+    	return "{\n\tisAlive: true\n}";
+    }
+}

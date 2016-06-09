920035bc2947144a06e7de2eafd716bddba5efeb
Brian <brianvanstone@gmail.com>
2016-06-09 16:08:36 -0400

    Cleaning up project properties and settings

--- src/main/java/tech/notpaper/mws/IdentifyCard.java
+++ src/main/java/tech/notpaper/mws/IdentifyCard.java
@@ -3,7 +3,6 @@ package tech.notpaper.mws;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Arrays;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -11,7 +10,6 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
